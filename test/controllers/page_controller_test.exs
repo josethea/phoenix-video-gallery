0661240697e26fef5dd9d46ce2fbe2a6f523d5ef
jthea <jthea0609@gmail.com>
2018-05-06 01:22:06 -0500

    edit home page, add navbar and custom errors

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Phoenixvideogallery.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Video Gallery!"
   end
 end
